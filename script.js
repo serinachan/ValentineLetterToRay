6e22e2a27406748b8e0f5660ba0aa90f129060fd
serinachan <alifahistighfarina@gmail.com>
2026-02-06 22:48:55 +0700

    Add a love letter typing animation in script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const message = `I don’t even know if you’ll ever read this.\nBut if you do…\n\n Ialways love you and love you and you omly you. We both tried. We both cared. I admire you, aku happy banget bisa ngerayain valentine sama kamu, yang kedua kali ini hehe.\n\nI hope then we can still celebrate together.\n\nAnyway, I hope you're doing okay \n\nILY 224 ❤️‍🔥 `;
+
+function showLetter() {
+  document.getElementById("introText").style.opacity = 0;
+  document.querySelector(".btn").style.display = "none";
+
+  setTimeout(() => {
+    const letterBox = document.getElementById("letterBox");
+    const typedText = document.getElementById("typedText");
+    letterBox.style.display = "block";
+    let i = 0;
+
+    function typeWriter() {
+      if (i < message.length) {
+        typedText.innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWriter, 30);
+      }
+    }
+
+    typeWriter();
+  }, 600);
+}
